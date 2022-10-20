74f2148803e23c2fba1778b8f046a8c7a6619778
AnghelaRodríguezBarrios <anghela.rodriguez@vallegrande.edu.pe>
2022-10-20 10:43:14 -0500

    Actualizacion del archivo de base de datos

--- dbMinimarket.sql
+++ dbMinimarket.sql
@@ -1,2 +1,18 @@
 /* Listar base de datos del servidor */
 SHOW DATABASES;
+
+/*Crear base de datos */
+CREATE DATABASE dbMinimarket;
+
+/* Poner en uso base de datos */
+USE dbMinimarket;
+
+/* Crear tabla */
+CREATE TABLE VENDEDOR(
+    CODVEND CHAR(6),
+    NOMVEND VARCHAR(50),
+    DOCVEND VARCHAR(10),
+    CELVEND CHAR(9),
+    DOMVEND VARCHAR(80),
+    PRIMARY KEY (CODVEND)
+);
